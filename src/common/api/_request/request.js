22f92e64a5b14a0ed449fc80fdb47d8371a41021
ethan <graf.ethan@gmail.com>
2024-08-12 21:52:25 -0400

    Make http requests work with PDS

--- src/common/api/_request/request.js
+++ src/common/api/_request/request.js
@@ -7,22 +7,21 @@ import { getAccessToken } from '../../helpers'
 –––––––––––––––––––––––––––––––––––––––––––––––––– */
 
 async function request(options, skipAuth) {
-  if (!CONSUMER_KEY) throw new Error('Invalid Auth Key')
-  if (!skipAuth) options.data.access_token = await getAccessToken()
+  //if (!CONSUMER_KEY) throw new Error('Invalid Auth Key')
 
-  options.data.consumer_key = CONSUMER_KEY
+  //options.data.consumer_key = CONSUMER_KEY
 
   const headers = new Headers({
     'X-Accept': 'application/json',
     'Content-Type': 'application/json',
   })
 
-  //?? Is there any way to access this anymore since we no longer use cookie/local storage
-  //?? We never set this parameter anywhere; propose we delete this block
-  const serverAuth = await getSetting('base_server_auth')
-  if (serverAuth) {
-    headers.append('Authorization', 'Basic ' + Base64.encode(serverAuth))
+  if (!skipAuth) {
+    const access_token = await getSetting('access_token')
+    console.log("ACCESS TOKEN", access_token);
+    headers.append('Authorization', 'Bearer ' + access_token);
   }
+
 
   const fetchSettings = {
     method: 'POST',
@@ -36,15 +35,15 @@ async function request(options, skipAuth) {
 }
 
 async function getRequest(options, skipAuth) {
-  if (!CONSUMER_KEY) throw new Error('Invalid Auth Key')
+  //if (!CONSUMER_KEY) throw new Error('Invalid Auth Key')
   if (!skipAuth) options.data.access_token = await getAccessToken()
 
-  options.data.consumer_key = CONSUMER_KEY
+  //options.data.consumer_key = CONSUMER_KEY
 
   const headers = new Headers({
     'X-Accept': 'application/json',
     'Content-Type': 'application/json',
-    'Authorization': 'Bearer ' + options.data.access_token,
+    'Authorization': 'Bearer ' + options.token
   })
 
   const fetchSettings = {
