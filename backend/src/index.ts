f62ce70cd8ba3181e5d3d122d30da76620719174
ethan <graf.ethan@gmail.com>
2024-10-05 20:03:02 -0400

    Listen to 0.0.0.0

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,6 @@ import app from './app';
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   /* eslint-disable no-console */
-  console.log(`Listening: http://localhost:${port}`);
+  console.log(`Listening: http://0.0.0.0:${port}`);
   /* eslint-enable no-console */
 });
